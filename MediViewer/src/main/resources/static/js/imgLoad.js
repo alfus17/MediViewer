f0d3fc525562e221656737df9ac22b100fb82449
limhyeonseoung <coldfela99@gmail.com>
2024-10-16 15:53:20 +0900

    imgLooad.js -> wwwc tool 추가

--- MediViewer/src/main/resources/static/js/imgLoad.js
+++ MediViewer/src/main/resources/static/js/imgLoad.js
@@ -1,23 +1,29 @@
-let element;
-
 document.addEventListener('DOMContentLoaded', () => {
-    // cornerstone 초기화
     cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
     cornerstoneWADOImageLoader.external.cornerstoneTools = cornerstoneTools;
-    
-    // 이미지를 넣을 요소 얻어오기
-    element = document.getElementById('dicomImage'); // 전역 변수에 할당
-    window.element = element; // window에 등록하여 다른 파일에서도 접근 가능
 
-    // 이미지 요소를 초기화
+    // cornerstone 및 DICOM 이미지 초기화
+    element = document.getElementById('dicomImage');
     cornerstone.enable(element);
-    
-    // 이미지 얻어오기
-    const imageId = 'wadouri:img/MR.1.2.392.200036.9116.4.1.6116.40033.5.3001.1.1152393810.dcm';
-    
+
+    const imageId = 'wadouri:img/CT.1.2.840.113619.2.55.3.604688252.641.1497007218.50.dcm';
+
     cornerstone.loadImage(imageId).then(image => {
         cornerstone.displayImage(element, image);
     }).catch(err => {
         console.log('이미지 로드 실패 : ', err);
+    });
+
+    // cornerstoneTools 초기화 및 WWWC Tool 추가
+    cornerstoneTools.init();
+    const WwwcTool = cornerstoneTools.WwwcTool;
+    cornerstoneTools.addTool(WwwcTool, {
+        configuration: { windowWidth: 400, windowCenter: 40 } // 기본 값 설정 추가
+    });
+
+    // WWWC Tool 활성화 버튼
+    document.getElementById('wwwcButton').addEventListener('click', () => {
+        cornerstoneTools.setToolActive('Wwwc', { mouseButtonMask: 1 });
+        console.log("WWWCTool 활성화 - 마우스 드래그로 이미지 밝기/대비를 조정해 보세요.");
     });
 });
