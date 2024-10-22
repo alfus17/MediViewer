6030498c01961e6be11ad3cfe30c1b5735c3291d
JangGwangJin <alfus9802@naver.com>
2024-10-23 00:52:44 +0900

    @gjjang 비동기 함수 분리
    @gjjang 비동기 함수 분리

--- MediViewer/src/main/resources/static/js/test/test.js
+++ MediViewer/src/main/resources/static/js/test/test.js
@@ -18,9 +18,43 @@ function testDetailAxios(studyKey) {
         })).catch(errors => {
             console.error("Error in axios.all: ", errors);
         });
-
-
-
         
     });
+}
+
+
+async function test (studyKey) {
+    try {
+        console.log("testDetailAxios_studyKey : ", studyKey);
+        
+        // 첫 번째 요청
+        const result = await axios.get(`/api/views/${studyKey}`);
+        console.log(result.data);
+        series.push(...result.data);
+
+        // URL 배열 생성
+        const urlList = series.map(seriesKey => `/api/views/${studyKey}/${seriesKey}`);
+        console.log("urlList : ", urlList);
+
+        // 병렬 요청 호출
+        await fetchSeriesData(urlList);
+        
+    } catch (error) {
+        console.error("Error in testDetailAxios: ", error);
+    }
+}
+
+async function fetchSeriesData(urlList) {
+    try {
+        // 병렬로 모든 URL에 대해 axios GET 요청 보내기
+        const responses = await Promise.all(urlList.map(url => axios.get(url)));
+
+        const dataList = responses.map(response => response.data);
+        dcmPathLists.push(...dataList);
+
+        console.log("dcmPathLists [] : ", dcmPathLists);
+        
+    } catch (error) {
+        console.error("Error in fetchSeriesData: ", error);
+    }
 }
